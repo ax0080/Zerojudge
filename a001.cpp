a7c16b7a79d236448a6e1475e3afed7bc442db1b
謝旻言 <78300584+ax0080@users.noreply.github.com>
2023-11-27 13:00:49 +0800

    Add files via upload

---
+++ a001.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<typeinfo>
+#include<iomanip>
+#include<sstream>
+
+
+using namespace std;
+
+
+
+int main(void) {
+	
+	
+	
+	
+	char str[100];
+
+	scanf("%s", str);
+	printf("hello, %s", str);
+	
+
+	
+	return 0;
+}
